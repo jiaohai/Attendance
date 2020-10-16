c58eaa16b38642f2511ab2ac43bebb51e88b30fd
jiaohai <937805950@qq.com>
2020-10-16 14:32:27 +0800

    updata branch

---
+++ src/addadmin/groupFiled.vue
@@ -0,0 +1,31 @@
+POST addgroup: {
+  name: '',
+  people: [],
+  department: [],
+  des: ''
+}
+
+GET groupList: [
+  {
+    id: 0,
+    name: '',
+    people: [],
+    department: [],
+    des: ''
+  },
+  {
+    id: 1,
+    name: '',
+    people: [],
+    department: [],
+    des: ''
+  }
+]
+
+PUT editGroup: {
+  id: 1,
+  name: '',
+  people: [],
+  department: [],
+  des: ''
+}
